12dd78b9f20d14aae830de00bf0d81461ce2db02
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-01-10 23:49:42 +0530

    Add files via upload

---
+++ 10-01-2026.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int a[50], b[50], c[100];
+    int n1, n2, i, j = 0;
+
+    printf("Enter size of array1: ");
+    scanf("%d", &n1);
+    printf("Enter elements: ");
+    for(i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter size of array2: ");
+    scanf("%d", &n2);
+    printf("Enter elements: ");
+    for(i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+
+    for(i = 0; i < n1; i++)
+        c[j++] = a[i];
+    for(i = 0; i < n2; i++)
+        c[j++] = b[i];
+
+    printf("Merged array: ");
+    for(i = 0; i < n1 + n2; i++)
+        printf("%d ", c[i]);
+
+    return 0;
+}
