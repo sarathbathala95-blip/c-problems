b5e45eaa67cc68fa7f48c530c539841a10dd8162
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-02-21 14:07:07 +0530

    Add files via upload

---
+++ c prob 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int a;
+   scanf("%d",&a);
+   if (a>0){
+   printf("positive");}
+    else if (a<0){
+    printf("negitive");}
+    else{
+    printf("no value");}
+    return 0;
+}
---
+++ prob3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   int a;
+   scanf("%d",&a);
+   if (a%5==0)
+   printf("number is divisible by 5.");
+   else 
+   printf("number is not divisible by 5.");
+    return 0;
+}
