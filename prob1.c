7a466e671ee86a52cc8eac804ee85ff65d765a14
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-02-25 15:15:14 +0530

    Add files via upload

--- prob1.c
+++ prob1.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-int main() {
-    int a=10;
-    int result;
-    result=a+a;
-    printf("%d",result);
-    return 0;
+int main(){
+
+int a;
+scanf("%d",&a);
+if (a%2==0)
+printf("its even");
+else
+printf("its odd");
+return 0;
+
 }
