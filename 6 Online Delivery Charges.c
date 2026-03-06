079bdbc7cd4819b69fff3d9357ca86eccb9cc04d
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-03-06 11:44:36 +0530

    Add files via upload

---
+++ 6 Online Delivery Charges.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int orderAmount,speedType;
+
+    scanf("%d",&orderAmount);
+    scanf("%d",&speedType);
+
+    switch(speedType)
+    {
+        case 1: printf("Delivery ₹50"); break;
+        case 2:
+            if(orderAmount<1000)
+                printf("Delivery ₹100");
+            else
+                printf("Delivery ₹0");
+            break;
+    }
+    return 0;
+}
