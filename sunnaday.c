87beaca95518a4e3d7f1cd4e6032f96b2d6f999f
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-03-07 11:17:05 +0530

    Add files via upload

---
+++ sunnaday.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    char day;
+    printf("enter the day = ");
+    scanf("%d",&day);
+    switch(day){
+
+case 1: printf("Sunday");
+    break;
+    case 2: printf("Monday");
+    break;
+    case 3: printf("Tuesday");
+    break;
+    case 4: printf("Wednesday");
+    break;
+    case 5: printf("Thursday");
+    break;  
+    case 6: printf("Friday");
+    break;
+case 7: printf("Satarday");
+    break;
+    default:
+    printf("Enter Valid Number");
+    }
+    return 0;
+}
