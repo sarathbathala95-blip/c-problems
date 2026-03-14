24c63794aee8315ae374dfa66800bdfdd7f61717
sarathbathala95-blip <sarathbathala95@gmail.com>
2026-03-14 11:01:57 +0530

    Add files via upload

---
+++ System Access.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int role,time;
+
+scanf("%d%d",&role,&time);
+
+switch(role){
+case 1: printf("Full Access"); break;
+
+case 2:
+if(time>=9 && time<=18) printf("Limited Access");
+else printf("Access Denied");
+}
+}
